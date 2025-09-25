2ea5714dc3f52d8b495b53bdf27136cb9bdf11a5
davidpfister <david.pfister@ypso-facto.com>
2025-09-25 09:44:59 +0200

    add tests

--- test/main.f90
+++ test/main.f90
@@ -8,9 +8,28 @@ TESTPROGRAM(main)
         integer :: unit, s
 
         call new(p, 'fpm.toml', 'json')
-        call p%display('test/test', '.json')
+        call p%display('test/test_json', '.json')
 
-        open(newunit=unit, file='test/test.json')
+        open(newunit=unit, file='test/test_json.json')
+        inquire(unit=unit, exist=exist, size=s)
+
+        EXPECT_TRUE(exist)
+        EXPECT_GT(s, 0)
+
+        close(unit, status='delete')
+    END_TEST
+
+    TEST('test_toml')
+        use modules_packages
+
+        type(package) :: p
+        logical :: exist
+        integer :: unit, s
+
+        call new(p, 'fpm.toml', 'toml')
+        call p%display('test/test_toml', '.toml')
+
+        open(newunit=unit, file='test/test_toml.toml')
         inquire(unit=unit, exist=exist, size=s)
 
         EXPECT_TRUE(exist)
@@ -27,9 +46,9 @@ TESTPROGRAM(main)
         integer :: unit, s
 
         call new(p, 'fpm.toml', 'circle')
-        call p%display('test/test', '.html')
+        call p%display('test/test_circle', '.html')
 
-        open(newunit=unit, file='test/test.html')
+        open(newunit=unit, file='test/test_circle.html')
         inquire(unit=unit, exist=exist, size=s)
 
         EXPECT_TRUE(exist)
@@ -46,9 +65,9 @@ TESTPROGRAM(main)
         integer :: unit, s
 
         call new(p, 'fpm.toml', 'force')
-        call p%display('test/test', '.html')
+        call p%display('test/test_force', '.html')
 
-        open(newunit=unit, file='test/test.html')
+        open(newunit=unit, file='test/test_force.html')
         inquire(unit=unit, exist=exist, size=s)
 
         EXPECT_TRUE(exist)
@@ -65,9 +84,28 @@ TESTPROGRAM(main)
         integer :: unit, s
 
         call new(p, 'fpm.toml', 'mermaid')
-        call p%display('test', '.html')
+        call p%display('test/test_mermaid', '.html')
 
-        open(newunit=unit, file='test.html')
+        open(newunit=unit, file='test/test_mermaid.html')
+        inquire(unit=unit, exist=exist, size=s)
+
+        EXPECT_TRUE(exist)
+        EXPECT_GT(s, 0)
+
+        close(unit, status='delete')
+    END_TEST
+
+    TEST('test_dot')
+        use modules_packages
+        
+        type(package) :: p
+        logical :: exist
+        integer :: unit, s
+
+        call new(p, 'fpm.toml', 'dot')
+        call p%display('test/test_dot', '.dot')
+
+        open(newunit=unit, file='test/test_dot.dot')
         inquire(unit=unit, exist=exist, size=s)
 
         EXPECT_TRUE(exist)
