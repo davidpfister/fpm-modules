7140e0374749d74328f397c6b2586a58e9b97f16
davidpfister <david.pfister@ypso-facto.com>
2025-11-04 23:34:12 +0100

    final commit

--- test_project/submodules/src/timestamp.f90
+++
@@ -1,8 +0,0 @@
-module logging_timestamp
-   use common_time, only: current_time
-   use logging_abstract, only: abstract_logger_t
-
-   implicit none(type, external)
-
-   private
-end module logging_timestamp
